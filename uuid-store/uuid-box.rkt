bac39377239b132d4d1701afc4afc14009240aed
Matias Eyzaguirre <matias@fortlogic.net>
2015-01-26 10:37:35 -0700

    Embedded tests inline.

--- uuid-store/uuid-box.rkt
+++ uuid-store/uuid-box.rkt
@@ -12,6 +12,16 @@
 (require/typed libuuid
                [uuid-generate (→ String)])
 
+(module+ test
+  (require (except-in typed/rackunit test-case))
+  (define-syntax-rule (test-case name body ...)
+    (begin body ...)))
+
+;(module+ test
+  (: make-simple-store (∀ (V) ((HashTable String V) → (UUID-Storeof (HashTable String V) V))))
+  (define (make-simple-store (store (make-hash)))
+    (#{make-uuid-store @ (HashTable String V) V} store hash-has-key? hash-ref hash-set!));)
+
 (struct (A) baked-uuid-store ([read : (→ String A)]
                               [write! : (→ String A Void)]))
 
@@ -41,11 +51,22 @@
                 (bake-uuid-store store)
                 (box (make-optional (make-ephemeron value value)))))))
 
+(module+ test
+  (test-case "make-uuid-box"
+    (fail "write test")))
+
 (: fetch-uuid-box (∀ (S A) (→ (UUID-Storeof S A) String (Option (UUID-Boxof A)))))
 (define (fetch-uuid-box store uuid)
   (if (uuid-store-contains? store uuid)
       (uuid-box uuid (bake-uuid-store store) (box (ann #f (Option (Ephemeronof A)))))
       #f))
+
+(module+ test
+  (test-case "fetch-uuid-box"
+    (let* ([uuid0 (uuid-generate)]
+                      [store (make-simple-store (make-hash `((,uuid0 . thing))))]
+                      #;[thing-box (fetch-uuid-box:hs store uuid0)])
+      (fail "write test"))))
 
 ;;; these uuid-box mutating functions might need to be made thread safe in the future...
 (: uuid-unbox (∀ (A) (→ (UUID-Boxof A) A)))
@@ -58,6 +79,18 @@
               (uuid-unbox/refresh ref-box)))
         (uuid-unbox/refresh ref-box))))
 
+(module+ test
+  (test-case "uuid-unbox"
+    (fail "write test")
+    (test-case "uuid-unbox fetched box"
+      (fail "write test"))
+    (test-case "uuid-unbox new box"
+      (fail "write test"))
+    (test-case "uuid-boxes reset during garbage collection"
+      (fail "write test"))
+    (test-case "uuid-boxes don't reset if there isn't a garbage collection"
+      (fail "write-test"))))
+
 (: uuid-unbox/refresh (∀ (A) (→ (UUID-Boxof A) A)))
 (define (uuid-unbox/refresh ref-box)
   (let ([value ((baked-uuid-store-read (uuid-box-store ref-box)) (uuid-box-uuid ref-box))])
@@ -65,7 +98,15 @@
               (make-ephemeron value value))
     value))
 
+(module+ test
+  (test-case "uuid-unbox/refresh"
+    (fail "write test")))
+
 (: set-uuid-box! (∀ (A) (→ (UUID-Boxof A) A Void)))
 (define (set-uuid-box! ref-box value)
   ((baked-uuid-store-write! (uuid-box-store ref-box)) (uuid-box-uuid ref-box) value)
   (set-box! (uuid-box-content ref-box) (make-ephemeron value value)))
+
+(module+ test
+  (test-case "set-uuid-box!"
+    (fail "write test")))
--- uuid-store/uuid-storage.rkt
+++ uuid-store/uuid-storage.rkt
@@ -10,6 +10,14 @@
          uuid-store-adaptor-decode
          adapt-uuid-store)
 
+(require/typed libuuid
+               [uuid-generate (→ String)])
+
+(module+ test
+  (require (except-in typed/rackunit test-case))
+  (define-syntax-rule (test-case name body ...)
+    (begin body ...)))
+
 (struct: (Store A) uuid-store ([storage : Store]
                                [contains?-proc : (→ Store String Boolean)]
                                [read-proc : (→ Store String A)]
@@ -24,17 +32,58 @@
 (define (make-uuid-store store contains reader writer)
   (uuid-store store contains reader writer))
 
+(module+ test
+  (: make-simple-store (∀ (V) ((HashTable String V) → (UUID-Storeof (HashTable String V) V))))
+  (define (make-simple-store (store (make-hash)))
+    (#{make-uuid-store @ (HashTable String V) V} store hash-has-key? hash-ref hash-set!)))
+
 (: uuid-store-contains? (∀ (S A) (→ (UUID-Storeof S A) String Boolean)))
 (define (uuid-store-contains? store uuid)
   ((uuid-store-contains?-proc store) (uuid-store-storage store) uuid))
+
+(module+ test
+  (test-case "uuid-store-contains?"
+    (let* ([uuid0 (uuid-generate)]
+           [uuid1 (uuid-generate)]
+           [store (make-simple-store (make-hash `((,uuid0 . important-thing))))])
+      (check-true (uuid-store-contains? store uuid0) "store doesn't have uuid0")
+      (check-false (uuid-store-contains? store uuid1) "store contains uuid1"))))
 
 (: uuid-store-read (∀ (S A) (→ (UUID-Storeof S A) String A)))
 (define (uuid-store-read store uuid)
   ((uuid-store-read-proc store) (uuid-store-storage store) uuid))
 
+(module+ test
+  (test-case "uuid-store-read"
+    (let* ([uuid0 (uuid-generate)]
+           [uuid1 (uuid-generate)]
+           [store (make-simple-store (make-hash `((,uuid0 . important-thing))))])
+      (check-eq? (uuid-store-read store uuid0) 'important-thing
+                 "store incorrectly reads data")
+      (check-exn #rx"hash-ref: no value found for key"
+                 (λ () (uuid-store-read store uuid1))
+                 "store read nonexistant data"))))
+
 (: uuid-store-write! (∀ (S A) (→ (UUID-Storeof S A) String A Void)))
 (define (uuid-store-write! store uuid value)
   ((uuid-store-write!-proc store) (uuid-store-storage store) uuid value))
+
+(module+ test
+  (test-case "uuid-store-write!"
+    (let* ([uuid0 (uuid-generate)]
+           [uuid1 (uuid-generate)]
+           [backing-store (make-hash `((,uuid0 . important-thing)))]
+           [store (make-simple-store backing-store)])
+      (uuid-store-write! store uuid0 'replacement-thing)
+      (check-equal? backing-store
+                    (make-hash `((,uuid0 . replacement-thing)))
+                    "failed to overwrite uuid0")
+      (check-false (uuid-store-contains? store uuid1) "modified other uuid")
+      (uuid-store-write! store uuid1 'other-thing)
+      (check-true (uuid-store-contains? store uuid1) "failed to add uuid1")
+      (check-equal? backing-store
+                    (make-hash `((,uuid0 . replacement-thing) (,uuid1 . other-thing)))
+                    "uuid1 failed to write correctly"))))
 
 (struct: (Src Res) uuid-store-adaptor ([encode-proc : (→ Res Src)]
                                        [decode-proc : (→ Src Res)]))
@@ -47,9 +96,37 @@
 (define (uuid-store-adaptor-encode adaptor res)
   ((uuid-store-adaptor-encode-proc adaptor) res))
 
+(module+ test
+  (: string->number* (→ String Number))
+  (define (string->number* str)
+    (let ([num (string->number str)])
+      (assert (number? num))
+      num))
+  
+  (test-case "uuid-store-adaptor-encode"
+    (let ([simple-adaptor (make-uuid-store-adaptor number->string string->number*)])
+      (check-equal? (uuid-store-adaptor-encode simple-adaptor 22/7)
+                    (number->string 22/7)
+                    "failed to encode correctly"))))
+
 (: uuid-store-adaptor-decode (∀ (Src Res) (→ (uuid-store-adaptor Src Res) Src Res)))
 (define (uuid-store-adaptor-decode adaptor src)
   ((uuid-store-adaptor-decode-proc adaptor) src))
+
+(module+ test
+  (test-case "uuid-store-adaptor-decode"
+    (let ([simple-adaptor (make-uuid-store-adaptor number->string string->number*)])
+      (check-equal? (uuid-store-adaptor-decode simple-adaptor "13/8")
+                    13/8
+                    "failed to decode correctly"))))
+
+(module+ test
+  (test-case "uuid-store-adaptor round trip"
+    (let ([simple-adaptor (make-uuid-store-adaptor number->string string->number*)])
+      (check-equal? (uuid-store-adaptor-decode simple-adaptor
+                                               (uuid-store-adaptor-encode simple-adaptor 22/7))
+                    22/7
+                    "uuid-store-adaptor doesn't round trip"))))
 
 (: adapt-uuid-store (∀ (S A B) (→ (UUID-Storeof S A) (uuid-store-adaptor A B) (UUID-Storeof S B))))
 (define (adapt-uuid-store store adaptor)
@@ -62,3 +139,16 @@
                 ((uuid-store-write!-proc store) s
                                                 uuid
                                                 (#{uuid-store-adaptor-encode @ A B} adaptor value)))))
+
+(module+ test
+  (test-case "adapt-uuid-store"
+    (let* ([uuid0 (uuid-generate)]
+           [uuid1 (uuid-generate)]
+           [store (make-simple-store (make-hash `((,uuid0 . "7"))))]
+           [simple-adaptor (make-uuid-store-adaptor number->string string->number*)]
+           [store* (adapt-uuid-store store simple-adaptor)])
+      (check-equal? (uuid-store-read store* uuid0) 7
+                    "adapted store fails to read")
+      (uuid-store-write! store* uuid1 55)
+      (check-equal? (uuid-store-read store uuid1) "55"
+                    "adapted store fails to write"))))
